b08c92529364df6fe956f29ff5be5f821c11d54b
geraldstanje <Gerald.Stanje@gmail.com>
2014-01-07 03:23:10 -0800

    added find k smallest element algorithm

---
+++ searching_algo/find_k_smallest_element.cpp
@@ -0,0 +1,65 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+/* Question: How to find the k smallest elements from an array.
+
+Find the k smallest elements from the set of n numbers in O(n) time complexity
+
+Solution: is based on Qickselect
+
+Time Complexity:
+Expected: O(n)
+Worst Case: O(n^2)
+*/
+
+int rand_partition(vector<int> &vec, int l, int r) {
+	int pivot_index = l + rand() % (r - l);
+	int pivot = vec[pivot_index];
+
+	while (l < r) {
+		while (l < vec.size() && vec[l] < pivot) { l++; }
+		while (r >= 0 && vec[r] > pivot) { r--; }
+
+		if (l < r) {
+			swap(vec[l], vec[r]);
+		}
+	}
+
+	return l;
+}
+
+int rand_selection(vector<int> &vec, int l, int r, int k) {
+	if (l == r) {
+		return vec[l];
+	}
+
+	int pivot_index = rand_partition(vec, l, r);
+
+	if (k == pivot_index) {
+		return vec[pivot_index];
+	}
+	else if (k > pivot_index) {
+		return rand_selection(vec, pivot_index + 1, r, k);
+	}
+	else if (k < pivot_index) {
+		return rand_selection(vec, l, pivot_index - 1, k);
+	}
+}
+
+int find_k_smallest_element(vector<int> &vec, int k) {
+	return rand_selection(vec, 0, vec.size() - 1, k);
+}
+
+int main() {
+	// your code goes here
+
+	srand(time(0));
+
+	vector<int> vec = { 10, 15, 2, 4, 1, 7, 5 };
+
+	for (int i = 0; i < vec.size(); i++) {
+		cout << find_k_smallest_element(vec, i) << endl;
+	}
+
+	return 0;
