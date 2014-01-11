e9ad9146e3c3bc3fd9ac2edc968d2aa890cc4293
geraldstanje <Gerald.Stanje@gmail.com>
2014-01-10 16:51:28 -0800

    added longest palindrome algorithm - not finished

---
+++ basic_algo/find_longest_palindrome.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <string>
+#include <sstream>
+using namespace std;
+
+/* Question:
+Find the longest palindrome in a input string.
+
+Example - aaabbaaaccdeqjncsdddmmmkkkmmmddd
+
+Answer - dddmmmkkkmmmddd
+*/
+
+string find_longest_palindrome(string s) {
+
+}
+
+int main() {
+	// your code goes here
+
+
+
+	return 0;
+}
