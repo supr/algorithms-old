46ad53b44fcbee237ec8148164f8c28f73f1b55f
geraldstanje <Gerald.Stanje@gmail.com>
2014-01-11 03:15:24 -0800

    implemented the longest palindrome algorithm

--- basic_algo/find_longest_palindrome.cpp
+++ basic_algo/find_longest_palindrome.cpp
@@ -1,6 +1,5 @@
 #include <iostream>
 #include <string>
-#include <sstream>
 using namespace std;
 
 /* Question:
@@ -11,14 +10,55 @@ Example - aaabbaaaccdeqjncsdddmmmkkkmmmddd
 Answer - dddmmmkkkmmmddd
 */
 
-string find_longest_palindrome(string s) {
+// searches for the largest palindrome for a given middle point left, right
+// compares each character on the left with the character on the right side
+void palindrome_search(const string &s, int left, int right, string &longest_palindrome) {
+	bool found = false;
 
+	while (left >= 0 && right < s.size()) {
+		if (s[left] == s[right]) {
+			left--;
+			right++;
+			found = true;
+		}
+		else {
+			break;
+		}
+
+	}
+
+	if (found) {
+		left++;
+		right--;
+
+		if (right - left + 1 > longest_palindrome.size()) {
+			longest_palindrome = s.substr(left, right - left + 1);
+		}
+	}
+}
+
+string find_longest_palindrome(const string &s) {
+	string longest_palindrome;
+
+	for (int i = 0; i < s.size(); i++) {
+		// pair elements in the middle of the palindrome
+		if (i < s.size() - 2 && s[i] == s[i + 1]) {
+			palindrome_search(s, i - 1, i + 2, longest_palindrome);
+		}
+
+		// single element in the middle of the palindrome
+		if (i < s.size() - 1) {
+			palindrome_search(s, i - 1, i + 1, longest_palindrome);
+		}
+	}
+
+	return longest_palindrome;
 }
 
 int main() {
 	// your code goes here
 
-
+	cout << find_longest_palindrome("aaabbaaaccdeqjncsdddmmmkkkmmmddd") << endl;
 
 	return 0;
 }
