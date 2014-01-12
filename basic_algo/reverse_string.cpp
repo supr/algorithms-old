caeda8a12db9f0e39a5ed0ea7d4111fcefe6007b
geraldstanje <Gerald.Stanje@gmail.com>
2014-01-12 03:46:09 -0800

    updated reverse string algorithm

--- basic_algo/reverse_string.cpp
+++ basic_algo/reverse_string.cpp
@@ -1,26 +1,40 @@
 #include <stdio.h>
 #include <string.h>
-using namespace std;
 
 /* Question:
 Implement a function void reverse(char* str) in C or C++ which reverses a nullterminated
 string.
-
-The solution is implemented in Ansi C
 */
 
-void swap(char &a, char &b) {
-	char tmp = b;
-	b = a;
-	a = tmp;
+void swap1(char *a, char *b) {
+	char tmp = *b;
+	*b = *a;
+	*a = tmp;
 }
 
-void my_reverse(char *str) {
+void my_reverse1(char *str) {
 	int j = strlen(str) - 1;
 
-	for (int i = 0; i < (strlen(str) - 1) / 2.0; i++) {
-		swap(str[i], str[j]);
+	for (int i = 0; i < strlen(str) / 2; i++) {
+		swap1(&str[i], &str[j]);
 		j--;
+	}
+}
+
+void swap2(char *a, char *b) {
+	char tmp = *b;
+	*b = *a;
+	*a = tmp;
+}
+
+void my_reverse2(char *str) {
+	char *start = str;
+	char *end = start + (strlen(start) - 1);
+
+	for (int i = 0; i < strlen(str) / 2; i++) {
+		swap2(start, end);
+		start++;
+		end--;
 	}
 }
 
@@ -30,8 +44,14 @@ int main() {
 	char str1[] = "Hello";
 	char str2[] = "Helloworld";
 
-	my_reverse(str1);
-	my_reverse(str2);
+	my_reverse2(str1);
+	my_reverse2(str2);
+
+	printf("%s\n", str1);
+	printf("%s\n", str2);
+
+	my_reverse1(str1);
+	my_reverse1(str2);
 
 	printf("%s\n", str1);
 	printf("%s\n", str2);
