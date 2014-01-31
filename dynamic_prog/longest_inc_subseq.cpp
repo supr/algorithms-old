1e8d074f5186e764822e8fdd01350ba28db95323
geraldstanje <Gerald.Stanje@gmail.com>
2014-01-31 15:23:52 -0800

    improved longest inc subsequence algorithm

--- dynamic_prog/longest_inc_subseq.cpp
+++ dynamic_prog/longest_inc_subseq.cpp
@@ -68,6 +68,45 @@ vector<int> find_longest_inc_subseq(vector<int> &vec) {
 	return subseq[subseq.size() - 1];
 }
 
+// Finds longest increasing subsequence...strictly increasing subsequence.O(n log k) algorithm. * /
+// explain: http://www.algorithmist.com/index.php/Longest_Increasing_Subsequence
+vector<int> find_longest_increasing_subsequence(vector<int> &a)
+{
+	vector<int> p(a.size());
+	vector<int> b;
+	int u, v;
+
+	if (a.empty()) return b;
+
+	b.push_back(0);
+
+	for (size_t i = 1; i < a.size(); i++) {
+		// If next element a[i] is greater than last element of current longest subsequence a[b.back()], just push it at back of "b" and continue
+		if (a[b.back()] < a[i]) {
+			p[i] = b.back();
+			b.push_back(i);
+			continue;
+		}
+
+		// Binary search to find the smallest element referenced by b which is just bigger than a[i]
+		// Note : Binary search is performed on b (and not a). Size of b is always <=k and hence contributes O(log k) to complexity.    
+		for (u = 0, v = b.size() - 1; u < v;) {
+			int c = (u + v) / 2;
+			if (a[b[c]] < a[i]) u = c + 1; else v = c;
+		}
+
+		// Update b if new value is smaller then previously referenced value 
+		if (a[i] < a[b[u]]) {
+			if (u > 0) p[i] = b[u - 1];
+			b[u] = i;
+		}
+	}
+
+	for (u = b.size(), v = b.back(); u--; v = p[v]) b[u] = v;
+
+	return b;
+}
+
 int main() {
 	// your code goes here
 
