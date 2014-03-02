7a82072d1b4f7f67b4f45b83844fd27c03e2eb7e
geraldstanje <Gerald.Stanje@gmail.com>
2014-03-01 17:44:13 -0800

    fixed bug in quick select algorithm

--- sorting_algo/find_top_k_elements.cpp
+++ sorting_algo/find_top_k_elements.cpp
@@ -13,39 +13,41 @@ Option 2: Using Selection algorithm (first finding kth largest element and then
 sorting the elements < k). Complexity O(k log k).
 */
 
-int rand_select(vector<int> &vec, int left, int right) {
-	int r = left + rand() % (right - left);
-	int pivot = vec[r];
-	
-	while(true) {
-		while(vec[left] < pivot) { left++; }
-		while(vec[right] > pivot) { right--; }
-		
-		if(left > right) {
-			break;
-		}
-		
-		swap(vec[left++], vec[right--]);
-	}	
-	
-	return left;
+int partition(vector<int> &vec, int left, int right) {
+    int pivot_index = right + rand() % (right - right + 1);
+    int pivot = vec[pivot_index];
+
+    swap(vec[pivot_index], vec[right]);  // Move pivot to end
+    int store_index = left;
+    
+    for(int i = left; i < right; i++) {
+    	if(vec[i] <= pivot) {
+        	swap(vec[store_index], vec[i]);
+            store_index++;
+    	}
+    }
+    
+    swap(vec[right], vec[store_index]);  // Move pivot to its final place
+    return store_index;
 }
 
 int quick_select(vector<int> &vec, int left, int right, int k) {
-	if(left == right) {
-		return vec[left];
-	}
-	
-	int pivot = rand_select(vec, left, right);
-	if(pivot == k) {
-		return vec[k];
-	}
-	else if(pivot < k) {
-		return quick_select(vec, pivot+1, right, k); 
-	}
-	else if(pivot > k) {
-		return quick_select(vec, left, pivot-1, k); 
-	}
+    if (left == right) { 
+        return vec[left];
+    }
+
+    int pivot_index = partition(vec, left, right);
+    
+    // The pivot is in its final sorted position
+    if(k == pivot_index) {
+    	return vec[k];
+    }
+    else if(k < pivot_index) {
+    	return quick_select(vec, left, pivot_index - 1, k);
+    }
+    else {
+        return quick_select(vec, pivot_index + 1, right, k);
+    }
 }
 
 vector<int> find_top_k_elements(vector<int> &vec, int k) {
@@ -62,7 +64,7 @@ vector<int> find_top_k_elements(vector<int> &vec, int k) {
 int main() {
 	// your code goes here
 	
-	vector<int> vec = {5,7,1,9,3,7,3,11,16};
+	vector<int> vec = {5,7,1,9,3,7,3,11,16,1};
 	vector<int> top_k_elements = find_top_k_elements(vec, 3);
 	
 	for_each(top_k_elements.begin(), top_k_elements.end(), [](int val) { cout << val << ' '; });
