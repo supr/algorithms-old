8e72a8c97c13cdfa8b907e8d8da5fae294219ec8
geraldstanje <Gerald.Stanje@gmail.com>
2014-03-01 20:06:44 -0800

    implemented get_stack_grow_direction algorithm

---
+++ bit_manipulation/get_stack_grow_direction.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+using namespace std;
+
+/* Question 
+Write a function which determines if a stack grows up or downwards
+*/
+
+// true = stack grows upwards
+// false = stack grows downwards
+bool get_stack_grow_direction(int *p) {
+	int i;
+	
+	if(!p) {
+		get_stack_grow_direction(&i);
+	}
+	else if(&i > p) {
+		return true;
+	}
+	else if(&i < p) {
+		return false;
+	}
+}
+
+
+int main() {
+	// your code goes here
+	
+	bool dir = get_stack_grow_direction(NULL);
+	if(dir) {
+		cout << "stack grows upwards" << endl;
+	}
+	else {
+		cout << "stack grows downwards" << endl; 
+	}
+	
+	return 0;
+}
