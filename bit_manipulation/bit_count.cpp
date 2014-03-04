5037a14637352fb54f3cf84e4a3561650e6269cc
geraldstanje <Gerald.Stanje@gmail.com>
2014-03-04 12:08:34 -0800

    added comments

--- bit_manipulation/bit_count.cpp
+++ bit_manipulation/bit_count.cpp
@@ -26,10 +26,44 @@ unsigned int bit_count2(unsigned int num) {
   
   while(num) {
     ++count;
-    num &= (num - 1);
-  }
+    num = num & (num - 1); // If you subtract -1 from a given number it will convert the last "1" bit to "0"
+  }                        // followed by continuous tail of 1, remove 1's tail by taking "&"
   
   return count;
+}
+
+// The best method for counting bits in a 32-bit integer v is the following:
+// The best bit counting method takes only 12 operations, which is the same as the lookup-table method, 
+// but avoids the memory and potential cache misses of a table. It is a hybrid between the purely parallel 
+// method above and the earlier methods using multiplies (in the section on counting bits with 64-bit instructions), 
+// though it doesn't use 64-bit instructions. The counts of bits set in the bytes is done in parallel, and the sum 
+// total of the bits set in the bytes is computed by multiplying by 0x1010101 and shifting right 24 bits.
+// reference: http://graphics.stanford.edu/~seander/bithacks.html
+unsigned int bit_count3(unsigned int num) {
+  unsigned int count = 0;
+  
+  num = num - ((num >> 1) & 0x55555555);                    // reuse input as temporary
+  num = (num & 0x33333333) + ((num >> 2) & 0x33333333);     // temp
+  count = ((num + (num >> 4) & 0xF0F0F0F) * 0x1010101) >> 24; // count
+  
+  return count;
+}
+
+unsigned int count_bits_float(float num) {
+  union u {
+      float num;
+      int i;
+  } u1;
+  u1.num = num; // floating number to check
+ 
+  unsigned int num_bits = 0;
+
+  while (u1.i) {
+      num_bits++;
+      u1.i = u1.i & (u1.i-1);
+  }
+ 
+	return num_bits;
 }
 
 int main() 
