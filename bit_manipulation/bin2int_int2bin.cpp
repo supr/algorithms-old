c78054982ef48b5e60096a2d8f12d4a27a27933f
geraldstanje <Gerald.Stanje@gmail.com>
2014-03-10 18:02:19 -0700

    added restriction to handle only pos. numbers

--- bit_manipulation/bin2int_int2bin.cpp
+++ bit_manipulation/bin2int_int2bin.cpp
@@ -10,9 +10,9 @@ Implement an algoritm to convert an integer number into a binary string
 Implement an algorithm to convert an binary string into an integer number
 */
 
-string int2bin(int number) {
+string int2bin(unsigned int number) {
 	string bin_str;
-	int index = log2(number);
+	unsigned int index = log2(number);
 	bin_str.resize(index + 1);
 	
 	while(number) {
@@ -23,9 +23,9 @@ string int2bin(int number) {
 	return bin_str;
 }
 
-int bin2int(string bin_str) {
-	int digit = 0;
-	int value = 0;
+unsigned int bin2int(string bin_str) {
+	unsigned int digit = 0;
+	unsigned int value = 0;
 
 	for (int i = bin_str.size() - 1; i >= 0; i--) {
 		if (bin_str[i] == '1') {
