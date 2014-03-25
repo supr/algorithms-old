4c0e5abf7d9f1ff5aecfd7b9e7af4793e99bb045
geraldstanje <Gerald.Stanje@gmail.com>
2014-03-24 22:54:03 -0700

    formatted code

--- data_structures/stack_question.cpp
+++ data_structures/stack_question.cpp
@@ -1,37 +1,26 @@
 #include <stdio.h>
 
-
-
 /* Question
 What is the output of the following code
 */
 
 void foo(void) {
-    
-	int a;
-    
-	printf("%d\n", a++);
-
+  int a;
+  printf("%d\n", a++);
 }
 
 
 
 void bar(void) {
-    
-	int b = 42;
-
+  int b = 42;
 }
 
 
 
 int main(void) {
-    
-	bar();
-    
-	foo();
-    
-	foo();
+  bar();
+  foo();
+  foo();
 
-	return 0;
-
+  return 0;
 }
