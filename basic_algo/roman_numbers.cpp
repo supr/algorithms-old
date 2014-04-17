a377fdab5fa9294a39d02d9e4ace2707ecc50df5
geraldstanje <Gerald.Stanje@gmail.com>
2014-04-17 12:11:00 +0200

    implemented a second version of the algorithm

--- basic_algo/roman_numbers.cpp
+++ basic_algo/roman_numbers.cpp
@@ -1,10 +1,8 @@
 #include <iostream>
 #include <string>
 #include <unordered_map>
+#include <cassert>
 using namespace std;
-
-int roman_to_integer(const string &str);
-
 
 bool is_valid_roman_string(const string &str, unordered_map<char, int> &ht) {
 	if (str.size() < 1) {
@@ -51,13 +49,56 @@ int roman_to_integer(const string &str) {
 	return number;
 }
 
-int main(int argc, char** argv)
-{
-	assert(1066, roman_to_integer("MLXVI"));
-	assert(-1, roman_to_integer("ZMLXVI"));
-	assert(6, roman_to_integer("VI"));
-	assert(1, roman_to_integer("I"));
-	assert(-1, roman_to_integer(""));
-	assert(-1, roman_to_integer("123123"));
-	assert(100, roman_to_integer("VIX"));
+int roman_to_integer2(const string &str) {
+	int number = 0;
+	int digit = 0;
+	int prev = 0;
+
+	unordered_map<char, int> roman;
+	roman['M'] = 1000;
+	roman['D'] = 500;
+	roman['C'] = 100;
+	roman['L'] = 50;
+	roman['X'] = 10;
+	roman['V'] = 5;
+	roman['I'] = 1;
+
+	if (!is_valid_roman_string(str, roman)) {
+		return -1;
+	}
+	
+	for(int i = str.size() - 1; i >= 0; i--) {
+		digit = roman[str[i]];
+		
+		if(prev > digit) {
+			digit = digit * -1;
+		}
+
+		number += digit;
+
+		prev = digit;
+	}
+
+	return number;
+}
+
+int main(int argc, char** argv) {
+	
+	assert(1066 == roman_to_integer("MLXVI"));
+	assert(-1 == roman_to_integer("ZMLXVI"));
+	assert(6 == roman_to_integer("VI"));
+	assert(1 == roman_to_integer("I"));
+	assert(-1 == roman_to_integer(""));
+	assert(-1 == roman_to_integer("123123"));
+	assert(109 == roman_to_integer("CIX"));
+
+	assert(1066 == roman_to_integer2("MLXVI"));
+	assert(-1 == roman_to_integer2("ZMLXVI"));
+	assert(6 == roman_to_integer2("VI"));
+	assert(1 == roman_to_integer2("I"));
+	assert(-1 == roman_to_integer2(""));
+	assert(-1 == roman_to_integer2("123123"));
+	assert(109 == roman_to_integer2("CIX"));
+
+	return 0;
 }
