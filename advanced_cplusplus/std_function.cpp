bed83e34dc9c8a5c467f22ffbd0e86bb6c7078bf
Gerald Stanje <Gerald.Stanje@gmail.com>
2014-05-05 21:27:41 +0200

    updated std_function example

--- advanced_cplusplus/std_function.cpp
+++ advanced_cplusplus/std_function.cpp
@@ -5,22 +5,36 @@ using namespace std;
 using namespace placeholders;
 
 /*
-With c++11 we have a new way to deal with function and member function pointers under the functional header, the std::function wrapper.
+With c++11 we have a new way to deal with function and member function pointers under the functional
+header, the std::function wrapper.
 */
 
 class Math {
 public:
-	void sum(int x, int y) {
-		cout << "sum: " << x + y << endl;
+	void sum1(int x, int y) {
+		cout << "sum1: " << x + y << endl;
 	}
 
+  static void diff(int x, int y) {
+    cout << "diff: " << x - y << endl;
+  }
 };
+
+static void sum2(int x, int y) {
+  cout << "sum2: " << x + y << endl;
+}
 
 int main() {
 	Math m;
 
-	std::function<void(int, int)> func = std::bind(&Math::sum, &m, _1, _2);
-	func(10, 20);
+	std::function<void(int, int)> func1 = std::bind(&Math::sum1, &m, _1, _2);
+	func1(10, 20);
+
+  std::function<void(int, int)> func2 = std::bind(&sum2, _1, _2);
+  func2(10, 20);
+
+  std::function<void(int, int)> func3 = std::bind(&Math::diff, _1, _2);
+  func3(10, 20);
 
 	return 0;
 }
