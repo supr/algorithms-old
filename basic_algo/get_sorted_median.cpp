48ddca6c6841655bbad6660cd81e8664b97efdf7
Gerald Stanje <Gerald.Stanje@gmail.com>
2014-06-17 22:20:54 +0200

    simplified code

--- basic_algo/get_sorted_median.cpp
+++ basic_algo/get_sorted_median.cpp
@@ -1,64 +1,65 @@
 #include <iostream>
 #include <vector>
+#include <deque>
 using namespace std;
 
-void update_median_elements(int &median_element_left, int &median_element_right, int median_index_left, int index, int curr_element) {
-  if(index < median_index_left) {
-    median_element_left = curr_element;
-  }
-  else {
-    median_element_right = curr_element;
-  }
-}
+/* Question:
+Get the Median of two sorted arrays.
+*/
 
 float get_median_of_two_sorted_vec(const vector<int> &vec1, const vector<int> &vec2) {
+  deque<int> median;
+  bool median_two_elements = false;
+  int len = vec1.size() + vec2.size();
   int i = 0;
   int j = 0;
-  int total_len = vec1.size() + vec2.size();
-  int median_index_left = (total_len / 2) + (total_len % 2);
-  int median_index_right = median_index_left;
+  int median_index = len / 2;
   int index = 0;
-  int median_element_left = 0;
-  int median_element_right = 0;
   
-  if((total_len % 2) == 0) {
-    median_index_right++;
+  // the number of all elements is even -> 2 median elements
+  if((len % 2) == 0) {
+    median_index += 1;
+    median_two_elements = true;
   }
   
-  while(index < median_index_right) {
-    if(i < vec1.size() && j < vec2.size()) {
-      if(vec1[i] < vec2[j]) {
-        update_median_elements(median_element_left, median_element_right, median_index_left, index, vec1[i]);
-        i++;
-      }
-      else if(vec2[j] < vec1[i]) {
-        update_median_elements(median_element_left, median_element_right, median_index_left, index, vec2[j]);
-        j++;
-      }
-      else {
-        update_median_elements(median_element_left, median_element_right, median_index_left, index, vec1[i]);
-        index++;
-        i++;
-        j++;      
-      }
+  for(int index = 0; index < len; index++) {
+    if(median.size() > 0) {
+      median.pop_front();
     }
-    else if(i < vec1.size()) {
-      update_median_elements(median_element_left, median_element_right, median_index_left, index, vec1[i]);
+    
+    if(vec1[i] < vec2[j]) {
+      median.push_back(vec1[i]);
       i++;
     }
-    else if(j < vec2.size()) {
-      update_median_elements(median_element_left, median_element_right, median_index_left, index, vec2[j]);
+    else if(vec1[i] > vec2[j]) {
+      median.push_back(vec2[j]);
       j++;
     }
-
-    index++;
+    else {
+      median.push_back(vec1[i]);
+      
+      if(median_two_elements) {
+        if(median.size() == 2) {
+          median.pop_front();
+        }
+        median.push_back(vec1[i]);  
+      }
+      
+      i++;
+      j++;
+      index++;
+    }
+    
+    if(index >= median_index) {
+      break;
+    }
+  }
+      
+  if(!median_two_elements) {
+    return median[0];
   }
   
-  if(median_index_left == median_index_right) {
-    return median_element_left;
-  }
-  
-  return (median_element_left + median_element_right) / 2.0;
+  return (median[0] + median[1]) / 2.0;
 }
 
 int main() {
@@ -68,6 +69,16 @@ int main() {
   vector<int> vec2 = {2, 3, 8, 10, 17, 22};
     
   cout << get_median_of_two_sorted_vec(vec1, vec2) << endl;
+ 
+  vector<int> vec3 = {1, 1, 3, 4};
+  vector<int> vec4 = {1, 2, 8, 9, 10};
+    
+  cout << get_median_of_two_sorted_vec(vec3, vec4) << endl; 
+
+  vector<int> vec5 = {1, 1, 3, 4};
+  vector<int> vec6 = {1, 2, 8, 9};
+    
+  cout << get_median_of_two_sorted_vec(vec5, vec6) << endl; 
   
   return 0;
 }
