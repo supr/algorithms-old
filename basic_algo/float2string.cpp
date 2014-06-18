33692a7db3c2aea930b237f2c86e1bce0ffa09c2
Gerald Stanje <Gerald.Stanje@gmail.com>
2014-06-18 18:11:51 +0200

    fixed bug

--- basic_algo/float2string.cpp
+++ basic_algo/float2string.cpp
@@ -11,42 +11,30 @@ float no.: 7.64
 output
 string: 7.64
 */
-
-string int_to_string(int val) {
-  int len = log10(val) + 1;
-  string str;
-  str.resize(len);
-  int index = len - 1;
   
-  while(index >= 0) {
-    str[index] = '0' + (val % 10);
-    val = val / 10;
-    index--;
-  }
+template <class T> 
+string to_string(T t) 
+{ 
+    stringstream ss; 
+    ss << t; 
+    return ss.str(); 
+} 
   
-  return str;
-}
-
-string float_to_string(float val) {
-  int integ = static_cast<int>(val);
-  int fract = 0;
-
-  val = val - integ;
-  while(val > 0) {
-    val = val * 10;
-    fract = (fract * 10) +  static_cast<int>(val);    
-    val = val - static_cast<int>(val);
-  }
+string float_to_string(float val, int n) 
+{ 
+    int integral = static_cast<int>(val); 
+    float fractional = val - integral; 
+    fractional = fractional * pow(10,n);
+    
+    string str = to_string<int>(integral) + 
+                 '.' + 
+                 to_string<int>(static_cast<int>(fractional)); 
   
-  string str = int_to_string(integ) + '.' + int_to_string(fract);
-
-  return str;
-}
-
-int main() {
-  // your code goes here
+    return str; 
+} 
   
-  cout << float_to_string(7.64) << endl;
-  
-  return 0;
+int main() 
+{ 
+    cout << float_to_string(7.64, 8) << endl; 
+    return 0;
 }
