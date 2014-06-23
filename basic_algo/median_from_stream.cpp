14f1946a1fae0812d26028c2473846ea669de65d
Gerald Stanje <Gerald.Stanje@gmail.com>
2014-06-23 23:55:06 +0200

    fixed bug

--- basic_algo/median_from_stream.cpp
+++ basic_algo/median_from_stream.cpp
@@ -1,97 +1,97 @@
 #include <iostream>
-#include <cmath>
 #include <vector>
+#include <deque>
 #include <queue>
+#include <algorithm>
 using namespace std;
 
-/*
+/* Question:
 Question: How to get the median from a stream of numbers at any time?
 The median is middle value of numbers. If the count of numbers is even, the
 median is defined as the average value of the two numbers in middle
+
+if the current element is less than maxheap.top, then insert it into maxheap.  
+if the current element is greater than minheap.top, then insert it into minheap.  
+If neither of those are true, insert it into whichever heap has smaller size.  
+Then, if necessary, pop an element from the smaller size heap and insert it 
+into the larger size heap, to keep their sizes within 1 of each other
 
 Time Complexity: O(log(n))
 Space Complexity: O(n)
 */
 
-class MedianFromStream {
+class MedianStream {
 private:
-	// stores all numbers smaller than the median
-	priority_queue<int, vector<int>, std::less<int>> pq_min;
-	// stores all number greater than the median
-	priority_queue<int, vector<int>, std::greater<int>> pq_max;
-
+  // stores all numbers smaller than the median
+  priority_queue<int, vector<int>, std::greater<int>> min_heap;
+  // stores all number greater than the median
+  priority_queue<int, vector<int>, std::less<int>> max_heap;
+  
 public:
-	float get_median(int number) {
-		if (pq_min.size() == 0) {
-			pq_min.push(number);
-		}
-		else if (pq_max.size() == 0) {
-			pq_max.push(number);
-
-			if (pq_max.top() < pq_min.top()) {
-				int tmp = pq_min.top();
-				pq_min.pop();
-				pq_min.push(pq_max.top());
-				pq_max.pop();
-				pq_max.push(tmp);
-			}
-		}
-		else {
-			// check if we need to insert the new number into the min heap
-			// the min heap stores the largest number in the top
-			if (number <= pq_min.top()) {
-				// check if the min heap size is larger than max heap size
-				// in this case we need to shift the top element from the min heap to the max heap
-				if (pq_min.size() > pq_max.size()) {
-					int largest_min = pq_min.top();
-					pq_min.pop();
-					pq_max.push(largest_min);
-				}
-
-				pq_min.push(number);
-			}
-			// check if we need to insert the new number into the max heap
-			// the max heap stores the smallest number in the top
-			else if (number >= pq_max.top()) {
-				// check if the max heap size is larger than min heap size
-				// in this case we need to shift the top element from the max heap to the min heap
-				if (pq_max.size() > pq_min.size()) {
-					int smallest_max = pq_max.top();
-					pq_max.pop();
-					pq_min.push(smallest_max);
-				}
-
-				pq_max.push(number);
-			}
-		}
-
-		// if min heap and max heap have the same size -> median is the average of both top elements
-		if (pq_min.size() == pq_max.size()) {
-			return (pq_min.top() + pq_max.top()) / 2.0;
-		}
-		// if min heap size is larger than the max heap -> median is the top element of min heap
-		else if (pq_min.size() > pq_max.size()) {
-			return pq_min.top();
-		}
-		// if max heap size is larger than the min heap -> median is the top element of max heap
-		return pq_max.top();
-	}
+  void insert(int value) {
+    if(min_heap.size() == 0 && max_heap.size() == 0) {
+      min_heap.push(value);
+    }
+    // if the current element is greater than minheap.top, then insert it into minheap.  
+    else if(value > min_heap.top()) {
+      if(min_heap.size() > max_heap.size()) {
+        int top = min_heap.top();
+        min_heap.pop();
+        max_heap.push(top);
+      }
+      min_heap.push(value);
+    }
+    // if the current element is less than maxheap.top, then insert it into maxheap. 
+    else if(value < max_heap.top()) {
+      if(max_heap.size() > min_heap.size()) {
+        int top = max_heap.top();
+        max_heap.pop();
+        min_heap.push(top);
+      }
+      max_heap.push(value); 
+    }
+    // If neither of those are true, insert it into whichever heap has smaller size.  
+    else {
+      if(max_heap.size() < min_heap.size()) {
+        max_heap.push(value);
+      }
+      else {
+        min_heap.push(value);
+      }
+    }
+  }
+  
+  float get_median() {
+      if(min_heap.size() > max_heap.size()) {
+        return min_heap.top();
+      }
+      else if(max_heap.size() > min_heap.size()) {
+        return max_heap.top();
+      }
+      return (min_heap.top() + max_heap.top()) / 2.0;
+  }
 };
 
 int main() {
-	vector<int> vec = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+  // your code goes here
 
-	MedianFromStream m1;
-	for (int i = 0; i < vec.size(); i++) {
-		cout << m1.get_median(vec[i]) << ' ';
-	}
-	cout << endl;
+  vector<int> vec1 = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+  
+  MedianStream m1; 
+  for (int i = 0; i < vec1.size(); i++) {
+  m1.insert(vec1[i]);
+  cout << m1.get_median() << ' ';
+  }
+  cout << endl;
 
-	MedianFromStream m2;
-	for (int i = vec.size() - 1; i >= 0; i--) {
-		cout << m2.get_median(vec[i]) << ' ';
-	}
-	cout << endl;
-
-	return 0;
+  vector<int> vec2 = { 1, 5, 3, 2, 8, 9 };
+  
+  MedianStream m2; 
+  for (int i = 0; i < vec2.size(); i++) {
+  m2.insert(vec2[i]);
+  cout << m2.get_median() << ' ';
+  }
+  cout << endl;
+  
+  return 0;
 }
