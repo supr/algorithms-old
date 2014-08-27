7fd4164836f23188834c8cc2c5cef1b1a852f1c7
Gerald Stanje <Gerald.Stanje@gmail.com>
2014-08-28 00:43:40 +0200

    formatted code

--- graph_algo/maze.cpp
+++ graph_algo/maze.cpp
@@ -1,9 +1,9 @@
 /*
 Question: Rat in a Maze
 Lets assume we have a rat maze as represented by the following NxM matrix 
-where S is the start location and F is the end location. 
+where S is the start location and E is the end location. 
 
-0 F 0 0 0 0 0 0 0
+0 E 0 0 0 0 0 0 0
 0 1 1 1 0 1 1 0 0
 0 1 0 1 0 1 0 0 0
 0 0 0 1 1 1 1 1 0
@@ -15,20 +15,20 @@ where S is the start location and F is the end location.
 0 0 0 0 0 0 0 0 0 
 
 0 Wall
-F End
+E End
 S Start
 1 path
 
-The idea (as with any rat maze) is to traverse from S to F. The matrix can 
+The idea (as with any rat maze) is to traverse from S to E. The matrix can 
 have only 0 and 1 as values. 1 represents a path that can be taken and 0 
 represents a blocked path. 
 
 We can make the following assumption: 
-S will always be (5,3) and F will always be (N,M). 
+S will always be (5,3) and E will always be (N,M). 
 
-As seen from above, there can be many paths from S to F. 
+As seen from above, there can be many paths from S to E. 
 
-How do we find the shortest (or longest) path from S to F without actually 
+How do we find the shortest (or longest) path from S to E without actually 
 traversing all the possible paths. 
 
 Please post (with proof/explantion) your algorithms. Also can you then think 
@@ -83,15 +83,15 @@ public:
                                  {0,0,0,0,0,0,0,0,0}}; 
 
 private:
-  void set_pos(const pos &p, char val) {
+  void setPos(const pos &p, char val) {
     vec[p.row][p.col] = val; 
   }
   
-  char get_pos(const int &row, const int &col) {
+  char getPos(const int &row, const int &col) {
     return vec[row][col];
   }
   
-  bool is_valid_pos(const int &row, const int &col) {
+  bool isValidPos(const int &row, const int &col) {
     if(row < 0 || row > max_row) {
       return false;
     }
@@ -110,48 +110,48 @@ public:
     max_row = vec[0].size();
   }
   
-  bool getLeft(const pos &curr_pos) {
-    if(!is_valid_pos(curr_pos.row, curr_pos.col - 1)) {
+  bool canMoveLeft(const pos &curr_pos) {
+    if(!isValidPos(curr_pos.row, curr_pos.col - 1)) {
       return false;
     }
     
-    if(get_pos(curr_pos.row, curr_pos.col - 1) == 0) {
+    if(getPos(curr_pos.row, curr_pos.col - 1) == 0) {
       return false;
     }
     
     return true;  
   }
   
-  bool getRight(const pos &curr_pos) {
-    if(!is_valid_pos(curr_pos.row, curr_pos.col + 1)) {
+  bool canMoveRight(const pos &curr_pos) {
+    if(!isValidPos(curr_pos.row, curr_pos.col + 1)) {
       return false;
     }
     
-    if(get_pos(curr_pos.row, curr_pos.col + 1) == 0) {
+    if(getPos(curr_pos.row, curr_pos.col + 1) == 0) {
       return false;
     }
     
     return true;  
   }
 
-  bool getUpper(const pos &curr_pos) {
-    if(!is_valid_pos(curr_pos.row - 1, curr_pos.col)) {
+  bool canMoveUp(const pos &curr_pos) {
+    if(!isValidPos(curr_pos.row - 1, curr_pos.col)) {
       return false;
     }
     
-    if(get_pos(curr_pos.row - 1, curr_pos.col) == 0) {
+    if(getPos(curr_pos.row - 1, curr_pos.col) == 0) {
       return false;
     }
     
     return true;
   }
   
-  bool getLower(const pos &curr_pos) {
-    if(!is_valid_pos(curr_pos.row + 1, curr_pos.col)) {
+  bool canMoveDown(const pos &curr_pos) {
+    if(!isValidPos(curr_pos.row + 1, curr_pos.col)) {
       return false;
     }
     
-    if(get_pos(curr_pos.row + 1, curr_pos.col) == 0) {
+    if(getPos(curr_pos.row + 1, curr_pos.col) == 0) {
       return false;
     }
     
@@ -167,7 +167,7 @@ public:
     }
   }
   
-  bool is_visited(const pos &p) {
+  bool isVisited(const pos &p) {
     if(visited[p.row][p.col] == 0) {
       return false;
     }
@@ -175,7 +175,7 @@ public:
     return true;
   }
   
-  void set_visited(const pos &p, bool flag) {
+  void setVisited(const pos &p, bool flag) {
     if(flag) {
       visited[p.row][p.col] = 1;
     }
@@ -184,31 +184,31 @@ public:
     }
   }
   
-  pos move_left(const pos &curr_pos) {
+  pos moveLeft(const pos &curr_pos) {
     pos new_pos = curr_pos;
     new_pos.col -= 1; 
     return new_pos;
   }
   
-  pos move_right(const pos &curr_pos) {
+  pos moveRight(const pos &curr_pos) {
     pos new_pos = curr_pos;
     new_pos.col += 1;
     return new_pos;
   }
   
-  pos move_upper(const pos &curr_pos) {
+  pos moveUp(const pos &curr_pos) {
     pos new_pos = curr_pos;
     new_pos.row -= 1;
     return new_pos;
   }
   
-  pos move_lower(const pos &curr_pos) {
+  pos moveDown(const pos &curr_pos) {
     pos new_pos = curr_pos;
     new_pos.row += 1;
     return new_pos;
   }
   
-  void reset_visited() {
+  void resetVisited() {
     for(int i = 0; i < visited.size(); i++) {
       for(int j = 0; j < visited[0].size(); j++) {
         visited[i][j] = 0;
@@ -217,7 +217,7 @@ public:
   }
 
   // iterative DFS
-  pos get_exit(const pos &start_pos) {
+  pos getExitIterative(const pos &start_pos) {
     pos tmp;
     stack<pos> q;
     q.push(start_pos);
@@ -230,27 +230,27 @@ public:
         break;
       }
       
-      if(!is_visited(tmp)) {
-        set_visited(tmp, true);
+      if(!isVisited(tmp)) {
+        setVisited(tmp, true);
       }
       else {
         continue;
       }
       
-      if(getLower(tmp)) {
-        pos new_pos = move_lower(tmp);
+      if(canMoveDown(tmp)) {
+        pos new_pos = moveDown(tmp);
         q.push(new_pos);
       }
-      if(getUpper(tmp)) {
-        pos new_pos = move_upper(tmp);
+      if(canMoveUp(tmp)) {
+        pos new_pos = moveUp(tmp);
         q.push(new_pos);
       }
-      if(getLeft(tmp)) {
-        pos new_pos = move_left(tmp);
+      if(canMoveLeft(tmp)) {
+        pos new_pos = moveLeft(tmp);
         q.push(new_pos);
       }
-      if(getRight(tmp)) {
-        pos new_pos = move_right(tmp);
+      if(canMoveRight(tmp)) {
+        pos new_pos = moveRight(tmp);
         q.push(new_pos);
       }
     }
@@ -259,43 +259,43 @@ public:
   }
   
   // recursive DFS
-  pos get_exit2(pos curr_pos) {
+  pos getExitRecursive(pos curr_pos) {
     pos exit_pos;
 
-    get_exit2_internal(curr_pos, exit_pos);
+    getExitRec(curr_pos, exit_pos);
 
     return exit_pos;
   }
 
-  void get_exit2_internal(pos curr_pos, pos &exit_pos) {
+  void getExitRec(pos curr_pos, pos &exit_pos) {
     if(curr_pos == end_pos) {
       exit_pos = curr_pos;
     }
     
-    set_visited(curr_pos, true);
+    setVisited(curr_pos, true);
       
-    if(getLower(curr_pos)) {
-      pos new_pos = move_lower(curr_pos);
-      if(!is_visited(new_pos)) {
-        get_exit2_internal(new_pos, exit_pos);
+    if(canMoveDown(curr_pos)) {
+      pos new_pos = moveDown(curr_pos);
+      if(!isVisited(new_pos)) {
+        getExitRec(new_pos, exit_pos);
       }
     }
-    if(getUpper(curr_pos)) {
-      pos new_pos = move_upper(curr_pos);
-      if(!is_visited(new_pos)) {
-        get_exit2_internal(new_pos, exit_pos);
+    if(canMoveUp(curr_pos)) {
+      pos new_pos = moveUp(curr_pos);
+      if(!isVisited(new_pos)) {
+        getExitRec(new_pos, exit_pos);
       }
     }
-    if(getRight(curr_pos)) {
-      pos new_pos = move_right(curr_pos);
-      if(!is_visited(new_pos)) {
-        get_exit2_internal(new_pos, exit_pos);
+    if(canMoveRight(curr_pos)) {
+      pos new_pos = moveRight(curr_pos);
+      if(!isVisited(new_pos)) {
+        getExitRec(new_pos, exit_pos);
       }
     }
-    if(getLeft(curr_pos)) {
-      pos new_pos = move_left(curr_pos);
-      if(!is_visited(new_pos)) {
-        get_exit2_internal(new_pos, exit_pos);
+    if(canMoveLeft(curr_pos)) {
+      pos new_pos = moveLeft(curr_pos);
+      if(!isVisited(new_pos)) {
+        getExitRec(new_pos, exit_pos);
       }
     }
  }
@@ -308,12 +308,12 @@ int main() {
   m.print();
   
   pos start_pos = pos(5,3);
-  pos exit_pos = m.get_exit(start_pos);
+  pos exit_pos = m.getExitIterative(start_pos);
   cout << "exit_pos: " << exit_pos.row << ", " << exit_pos.col << endl;
 
-  m.reset_visited();
+  m.resetVisited();
 
-  pos exit_pos2 = m.get_exit2(start_pos);
+  pos exit_pos2 = m.getExitRecursive(start_pos);
   cout << "exit_pos: " << exit_pos2.row << ", " << exit_pos2.col << endl;
   
   return 0;
