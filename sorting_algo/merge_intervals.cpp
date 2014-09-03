1e38f27b93f5f6a9d5f44429f8add0ae4675a248
Gerald Stanje <Gerald.Stanje@gmail.com>
2014-09-03 23:12:55 +0200

    formatted code

--- sorting_algo/merge_intervals.cpp
+++ sorting_algo/merge_intervals.cpp
@@ -66,20 +66,20 @@ int main() {
 	// your code goes here
 	
 	vector<interval> intervals1 = { {6,8}, {1,9}, {2,4}, {4,7} };
-    vector<interval> intervals1_merged = merge_intervals(intervals1);
-    for_each(intervals1_merged.begin(), intervals1_merged.end(), [](interval interv) { cout << interv.start << "," << interv.end << "; "; });
+  vector<interval> intervals1_merged = merge_intervals(intervals1);
+  for_each(intervals1_merged.begin(), intervals1_merged.end(), [](interval interv) { cout << interv.start << "," << interv.end << "; "; });
 	
 	cout << '\n';
 	
 	vector<interval> intervals2 = { {6,8},{1,3},{2,4},{4,7} };
-    vector<interval> intervals2_merged = merge_intervals(intervals2);
-    for_each(intervals2_merged.begin(), intervals2_merged.end(), [](interval interv) { cout << interv.start << "," << interv.end << "; "; });
+  vector<interval> intervals2_merged = merge_intervals(intervals2);
+  for_each(intervals2_merged.begin(), intervals2_merged.end(), [](interval interv) { cout << interv.start << "," << interv.end << "; "; });
     
-    cout << '\n';
+  cout << '\n';
     
-    vector<interval> intervals3 = { {1,3},{7,9},{4,6},{10,13} };
-    vector<interval> intervals3_merged = merge_intervals(intervals3);
-    for_each(intervals3_merged.begin(), intervals3_merged.end(), [](interval interv) { cout << interv.start << "," << interv.end << "; "; });
+  vector<interval> intervals3 = { {1,3},{7,9},{4,6},{10,13} };
+  vector<interval> intervals3_merged = merge_intervals(intervals3);
+  for_each(intervals3_merged.begin(), intervals3_merged.end(), [](interval interv) { cout << interv.start << "," << interv.end << "; "; });
     
 	return 0;
 }
