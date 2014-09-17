7d73f612ab8a2997392c33fc0fe9b8eccc8c991e
Gerald Stanje <Gerald.Stanje@gmail.com>
2014-09-17 23:46:12 +0200

    modified algo

--- basic_algo/find_longest_prefix.cpp
+++ basic_algo/find_longest_prefix.cpp
@@ -9,3 +9,47 @@ Flight
 Output: 
 Fl
 */
+
+#include <iostream>
+using namespace std;
+
+const int alphabet_size = 26;
+
+class Node {
+private:
+  Node *next;
+  bool isEnd;
+  int count;
+  
+public:
+  Node(): next(NULL), isEnd(false), count(0) {}
+};
+
+class trie {
+private:
+  Node *root[alphabet_size];
+  
+public:
+  trie() {
+    for(int i = 0; i < alphabet_size; i++) {
+      root[i] = new Node;
+    }
+  }
+  
+  void insert(string str) {
+    for (int i = 0; i < alphabet_size; i++) {
+      if (i == ('z' - str[i])) {
+        root[i]->count++;
+      } 
+    }
+    
+  }
+};
+
+int main() {
+  // your code goes here
+  
+  trie t;
+  
+  return 0;
+}
