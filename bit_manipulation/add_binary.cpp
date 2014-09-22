cab3ea755621461216f3925678b2bb65b2cdc7d3
Gerald Stanje <Gerald.Stanje@gmail.com>
2014-09-23 00:01:21 +0200

    modified algo

--- bit_manipulation/add_binary.cpp
+++ bit_manipulation/add_binary.cpp
@@ -13,19 +13,23 @@ b = "1"
 Return "100".
 */
 
+string zeroPadding(string s, int bits) {
+  for (int i = 0; i < bits; i++) {
+    s = "0" + s;
+  } 
+  
+  return s;
+}
+
 string add_binary(string in1, string in2) {
 	string out;
 	int diff = abs(in1.size() - in2.size());
 
 	if (in1.size() > in2.size()) {
-		for (int i = 0; i < diff; i++) {
-			in2 = '0' + in2;
-		}
+    in2 = zeroPadding(in2, diff);
 	}
 	else if (in2.size() > in1.size()) {
-		for (int i = 0; i < diff; i++) {
-			in1 = '0' + in1;
-		}
+    in1 = zeroPadding(in1, diff);
 	}
 
 	char digit;
@@ -70,74 +74,14 @@ string add_binary(string in1, string in2) {
 	return out;
 }
 
-string zeroPadding(string s, int bits) {
-  for (int i = 0; i < bits; i++) {
-    s = "0" + s;
-  } 
-  
-  return s;
-}
-
-class SumCarry {
-public:
-  char sum;
-  char carry;
-  SumCarry(char sum_, char carry_): sum(sum_), carry(carry_) {}
-};
-
-string addBinary(string a, string b) {
-  int len1 = a.size();
-  int len2 = b.size();
-  string result('0', max(len1,len2)+1); // allocate one more carry bit
-  char carry = '0';
-  char sum = '0';                     // a=0,b=0         // a=0,b=1
-  vector<vector<SumCarry>> andVec = {{SumCarry('0','0'), SumCarry('1','0')},
-                                      // a=1,b=0         // a=1,b=1
-                                     {SumCarry('1','0'), SumCarry('0','1')}};
-  // addVector for carry=1                     // a=0,b=0         // a=0,b=1
-  vector<vector<SumCarry>> andVecWithCarry = {{SumCarry('1','0'), SumCarry('1=0','1')},
-                                               // a=1,b=0         // a=1,b=1
-                                              {SumCarry('0','1'), SumCarry('1','1')}};  
-  if (len1 < len2) {
-    a = zeroPadding(a, len2 - len1);
-  } else if (len1 > len2) {
-    b = zeroPadding(b, len1 - len2);
-  }
-  
-  for (int i = a.size() - 1; i >= 0; i--) {
-    int indexX = a[i] - '0';
-    int indexY = b[i] - '0';
-    
-    if (carry == '0') {
-      auto p = andVec[indexX][indexY];
-      sum = p.sum;
-      carry = p.carry;
-    } else if (carry == '1') {
-      auto p = andVecWithCarry[indexX][indexY];
-      sum = p.sum;
-      carry = p.carry;
-    }
-    
-    result[i+1] = sum;
-  }
-  
-  if (carry == '1') {
-    result[0] = carry;
-  } else {
-    result.erase(result.begin(), result.begin()+1);
-  }
-  
-  return result;
-}
-
 int main() {
 	// your code goes here
 
 	cout << add_binary("11", "1") << endl;
 
-  cout << addBinary("1100", "111") << endl;
+  cout << add_binary("1100", "111") << endl;
 
-  cout << addBinary("1", "1") << endl;
+  cout << add_binary("1", "1") << endl;
 
 	return 0;
 }
