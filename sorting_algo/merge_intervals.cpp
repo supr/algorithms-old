938bdcd46007fc26259235ea37038fa4fb41bf4e
Gerald Stanje <Gerald.Stanje@gmail.com>
2014-10-13 23:45:38 +0200

    simplified algo

--- sorting_algo/merge_intervals.cpp
+++ sorting_algo/merge_intervals.cpp
@@ -10,49 +10,47 @@ into one and output the result which should have only mutually exclusive interva
 Let the intervals be represented as pairs of integers for simplicity. 
 */
 
-typedef struct interval {
+class interval {
+public:
   int start;
   int end;
-  interval(int start_, int end_): start(start_), end(end_) {}
-}interval;
 
-typedef struct comparison {
-  bool operator()(const interval &lhs, const interval &rhs) {
-    return lhs.start < rhs.start;
-  }
-}comparison;
+  interval(int s, int e): start(s), end(e) {}
+};
 
-vector<interval> merge_intervals(vector<interval> &vec) {
+vector<interval> merge_intervals(vector<interval> &r) {
   vector<interval> out;
-  stack<interval> s;
+  stack<interval> s; 
   
-  if(vec.size() == 0) {
+  if(r.size() == 0) {
     return out;
   }
-  else if(vec.size() == 1) {
-    out = vec;
+  else if(r.size() == 1) {
+    out = r;
     return out;
   }
-  
-  std::sort(vec.begin(), vec.end(), comparison());
-  s.push(vec[0]);
-  
-  for(int i = 1; i < vec.size(); i++) {
-    interval top = s.top();
-    
-    if(vec[i].start > top.end) {
-      s.push(vec[i]);
-    }
-    else if(vec[i].start <= top.end) {
-      s.pop();
-      
-      if(vec[i].end > top.end) {
-        top.end = vec[i].end;
+
+  std::sort(r.begin(), r.end(), [](const interval &r1, const interval &r2) { return r1.start < r2.start; });
+
+  for (auto e : r) {
+    if (s.empty()) {
+      s.push(e);
+    } else {
+      auto top = s.top();
+
+      // check for overlapping
+      if(e.start <= top.end) {
+        top.end = max(e.end, top.end);
+        
+        s.pop();
+        s.push(top);
+      } else {
+        s.push(e);
       }
-      
-      s.push(top);
-    }
+    }   
   }
+  
+  r.clear();
   
   while(!s.empty()) {
     out.push_back(s.top());
@@ -65,21 +63,21 @@ vector<interval> merge_intervals(vector<interval> &vec) {
 int main() {
   // your code goes here
   
-    vector<interval> intervals1 = { {6,8}, {1,9}, {2,4}, {4,7} };
-    vector<interval> intervals1_merged = merge_intervals(intervals1);
-    for_each(intervals1_merged.begin(), intervals1_merged.end(), [](interval interv) { cout << interv.start << "," << interv.end << "; "; });
+  vector<interval> intervals1 = { {6,8}, {1,9}, {2,4}, {4,7} };
+  vector<interval> intervals1_merged = merge_intervals(intervals1);
+  for_each(intervals1_merged.begin(), intervals1_merged.end(), [](interval interv) { cout << interv.start << "," << interv.end << "; "; });
   
   cout << '\n';
   
   vector<interval> intervals2 = { {6,8},{1,3},{2,4},{4,7} };
-    vector<interval> intervals2_merged = merge_intervals(intervals2);
-    for_each(intervals2_merged.begin(), intervals2_merged.end(), [](interval interv) { cout << interv.start << "," << interv.end << "; "; });
+  vector<interval> intervals2_merged = merge_intervals(intervals2);
+  for_each(intervals2_merged.begin(), intervals2_merged.end(), [](interval interv) { cout << interv.start << "," << interv.end << "; "; });
     
-    cout << '\n';
+  cout << '\n';
     
-    vector<interval> intervals3 = { {1,3},{7,9},{4,6},{10,13} };
-    vector<interval> intervals3_merged = merge_intervals(intervals3);
-    for_each(intervals3_merged.begin(), intervals3_merged.end(), [](interval interv) { cout << interv.start << "," << interv.end << "; "; });
+  vector<interval> intervals3 = { {1,3},{7,9},{4,6},{10,13} };
+  vector<interval> intervals3_merged = merge_intervals(intervals3);
+  for_each(intervals3_merged.begin(), intervals3_merged.end(), [](interval interv) { cout << interv.start << "," << interv.end << "; "; });
     
   return 0;
 }
