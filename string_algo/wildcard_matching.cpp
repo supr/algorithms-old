c96e1aa6ba3eb3dcdfd92e4cacac01f4e9cae02a
Gerald Stanje <Gerald.Stanje@gmail.com>
2014-11-11 23:46:07 +0100

    added new question

---
+++ string_algo/wildcard_matching.cpp
@@ -0,0 +1,12 @@
+/* Question:
+String matching where one string contains wildcard characters
+Given two strings where first string may contain wild card characters and second 
+string is a normal string. Write a function that returns true if the two strings 
+match. The following are allowed wild card characters in first string.
+
+* --> Matches with 0 or more instances of any character or set of characters.
+? --> Matches with any one character.
+For example, “g*ks” matches with “geeks” match. And string “ge?ks*” matches with 
+“geeksforgeeks” (note ‘*’ at the end of first string). But “g*k” doesn’t match with 
+“gee” as character ‘k’ is not present in second string.
+*/
