ca018f95f2b048aebed07ba5323aa182883dbafc
Gerald Stanje <Gerald.Stanje@gmail.com>
2014-11-13 23:54:49 +0100

    added new question

---
+++ array_algo/find_top_5_elements.cpp
@@ -0,0 +1,20 @@
+/*Question:
+there are three shorted int Array 
+A1 : {2,4,9,16,19,68,78} 
+A2:{4,8,67,106,109,115,120} 
+A3:{9,15,19,108,119,130,190} 
+
+write an efficient algo to get the top 5 largest number from these three arrays. 
+
+Function would be like 
+
+vector<int> top5LargestNumber(const vector<int> &A1, const vector<int> &A2, const vector<int> &A3)
+*/
+
+#include <iostream>
+using namespace std;
+
+int main() {
+  // your code goes here
+  return 0;
+}
