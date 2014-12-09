65471990e9b996c577cc70c6c149da4c46055356
Gerald Stanje <Gerald.Stanje@gmail.com>
2014-12-09 23:57:29 +0100

    added new algo

---
+++ string_algo/headtail.cpp
@@ -0,0 +1,5 @@
+/*
+Given two English words of the same length, say, "HEAD" and "TAIL", come up with a 
+sequence of valid English words, starting with "HEAD", and ending with "TAIL", such 
+that each word is formed by changing a single letter of the previous word.
+*/
