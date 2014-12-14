ee0f9261dc8bd1266c7a4a0065e58f1d8cf80889
Gerald Stanje <Gerald.Stanje@gmail.com>
2014-12-14 19:00:17 +0100

    added new algo

--- array_algo/find_top_5_elements.cpp
+++ array_algo/find_top_5_elements.cpp
@@ -9,12 +9,17 @@ write an efficient algo to get the top 5 largest number from these three arrays.
 
 Function would be like 
 
+sorted arrays:
 vector<int> top5LargestNumber(const vector<int> &A1, const vector<int> &A2, const vector<int> &A3)
+
+unsorted arrays:
+vector<int> top5LargestNumber_unsorted(const vector<int> &A1, const vector<int> &A2, const vector<int> &A3)
 */
 
 #include <iostream>
 #include <vector>
 #include <algorithm>
+#include <queue>
 using namespace std;
 
 vector<int> top5LargestNumber(vector<int> A1, vector<int> A2, vector<int> A3) {
@@ -55,6 +60,47 @@ vector<int> top5LargestNumber(vector<int> A1, vector<int> A2, vector<int> A3) {
   return top;
 }
 
+struct comparison {
+  bool operator()(int a, int b) {
+    return a > b;
+  } 
+};
+
+void insert(priority_queue<int, vector<int>, comparison> &pq, int value) {
+  if (pq.size() < 5) {
+    pq.push(value);
+  } else {
+    if (value > pq.top()) {
+      pq.pop();
+      pq.push(value);
+    }
+  }
+}
+
+vector<int> top5LargestNumber_unsorted(const vector<int> &A1, const vector<int> &A2, const vector<int> &A3) {
+  priority_queue<int, vector<int>, comparison> pq;
+  vector<int> topElements;
+  
+  for (int i = 0; i < A1.size(); i++) {
+    insert(pq, A1[i]);
+  }
+  
+  for (int i = 0; i < A2.size(); i++) {
+    insert(pq, A2[i]);
+  }
+  
+  for (int i = 0; i < A3.size(); i++) {
+    insert(pq, A3[i]);
+  }
+  
+  while(!pq.empty()) {
+    topElements.push_back(pq.top());
+    pq.pop();
+  }
+  
+  return topElements;
+}
+
 int main() {
   // your code goes here
 
@@ -64,7 +110,19 @@ int main() {
 
   vector<int> top = top5LargestNumber(A1, A2, A3);
 
-  for(auto e : top) {
+  for(auto const&  e : top) {
+    cout << e << ' ';
+  }
+
+  cout << '\n';
+
+  vector<int> A4 = {4,2,9,78,19,68,16};
+  vector<int> A5 = {120,8,109,106,67,115,4};
+  vector<int> A6 = {130,9,190,108,119,15,19};
+
+  vector<int> top2 = top5LargestNumber_unsorted(A4, A5, A6);
+
+  for(auto const&  e : top2) {
     cout << e << ' ';
   }
 
