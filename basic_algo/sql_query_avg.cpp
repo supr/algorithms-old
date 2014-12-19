f0a9e57d4710bee2e0ac661d22719c2ad0176374
Gerald Stanje <Gerald.Stanje@gmail.com>
2014-12-19 17:54:18 +0100

    added comments

--- basic_algo/sql_query_avg.cpp
+++ basic_algo/sql_query_avg.cpp
@@ -5,6 +5,36 @@
 #include <limits>
 using namespace std;
 
+// under/overflow detection:
+// - You can check if the addition is safe before you do it:
+//   e.g. overflow: if (sum > 0 && sum > (numeric_limits<int>::max() - c1[i]))
+//
+// - an other option is checking the overflow flag with intrinsics -> they're compiler intrinsics
+//   meaning they'll emit a certain assembly instruction, depending on your architecture
+//   your compiler already knows when an overflow occurred
+//   it's a bit in the status flag, called the carry flag
+//   (i.e. there was a carry out of the last adder)
+//   A new set of built-in functions for arithmetics with overflow checking has been added: 
+//   __builtin_add_overflow, __builtin_sub_overflow and __builtin_mul_overflow and for compatibility 
+//   with clang also other variants.
+//
+// - use inline asm is also possible:
+//   "setc" means "set this memory location if the carry bit is 1"
+//   setc eax means set eax to be 1 if carry bit was 1 in the status flag
+//
+//   #define add64(a,b,c)                            \
+//   do                                              \
+//   {                                             \
+//      __asm__ __volatile__                        \
+//        (                                         \
+//        "add %2, %1 \n"                      \
+//        "setc  %0     \n"                      \
+//        : "=q" (c), "+q" (a)                     \
+//        : "q" (b)                                \
+//        );                                       \
+//   }                                             \
+//   while (0)
+
 int sql_query_avg_avg(const vector<int> &c1, const vector<string> &c2, const string &queryStr) {
   int sum = 0;
   int count = 0;
