a06f9f558c4224cbc996a3788e8590f0870946e1
Gerald Stanje <Gerald.Stanje@gmail.com>
2014-12-20 17:54:20 +0100

    added comments

--- bit_manipulation/is_little_endian.cpp
+++ bit_manipulation/is_little_endian.cpp
@@ -7,6 +7,20 @@ Write a function that determines whether a computer is big-endian
 or little-endian.
 */
   
+/*
+Little and big endian are two ways of storing multibyte data-types ( int, float, etc). 
+In little endian machines, last byte of binary representation of the multibyte data-type 
+is stored first. 
+On the other hand, in big endian machines, first byte of binary representation of the 
+multibyte data-type is stored first.
+
+Suppose integer is stored as 4 bytes: 0x12345678
+Memory Address: 0x10 0x11 0x12 0x13
+Little Endian:  78   56   34   12
+Big Endian:     12   34   56   78
+
+*/
+
 bool is_little_endian() { 
     union { 
         uint32_t idata; 
