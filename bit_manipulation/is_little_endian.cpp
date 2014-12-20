5335ef052650a9e5e32eb4ec2d310daf47d00917
Gerald Stanje <Gerald.Stanje@gmail.com>
2014-12-20 17:47:38 +0100

    modified code

--- bit_manipulation/is_little_endian.cpp
+++ bit_manipulation/is_little_endian.cpp
@@ -1,30 +1,30 @@
 #include <iostream>
+#include <cstdint>
 using namespace std;
 
 /* Question: 
-Write a C function that determines whether a computer is big-endian
+Write a function that determines whether a computer is big-endian
 or little-endian.
 */
-
-bool is_little_endian() {
-	union {
-		int integer;
-		char single_byte;
-	}endian_test;
-	
-	endian_test.integer = 0x1020;
-	
-	if(endian_test.single_byte == 0x20) {
-		return true;
-	}
-	
-	return false;
-}
-
-int main() {
-	// your code goes here
-	
-	cout << is_little_endian() << endl;
-	
-	return 0;
+  
+bool is_little_endian() { 
+    union { 
+        uint32_t idata; 
+        char cdata[4]; 
+    }endian_test; 
+    
+    endian_test.idata = 0x12345678; 
+    
+    if(endian_test.cdata[0] == 0x78) { 
+        return true; 
+    } 
+    
+    return false; 
+} 
+  
+int main() {    
+    // your code goes here
+    cout << is_little_endian() << endl; 
+    
+    return 0; 
 }
