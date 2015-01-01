3d0d3f7e7e9dfc7e8649455cc6fa7616b5e201e0
Gerald Stanje <Gerald.Stanje@gmail.com>
2015-01-01 14:11:09 +0100

    added std::string version

--- basic_algo/int2str.cpp
+++ basic_algo/int2str.cpp
@@ -4,7 +4,7 @@
 using namespace std;
 
 /*
-Question: Please write an algorithm to convert an Integer (int) to a String (char*)
+Question: Please write an algorithm to convert an Integer (int) to a String (char*) / std::string
 */
 
 char *int2str(int num) {
@@ -37,6 +37,32 @@ char *int2str(int num) {
 	}
 
 	return str;
+}
+
+string IntToString(int number) {
+  bool isNegative = false;
+  int index = 0;
+  string str; // or string str(index + 1, '0');
+  
+  if (number < 0) {
+    isNegative = true;
+    number = abs(number);
+    index = 1;
+  }
+  
+  index += log10(number);
+  str.resize(index+1);
+  
+  while (number) {
+    str[index--] = '0' + (number % 10);
+    number /= 10;
+  }
+  
+  if (isNegative) {
+    str[0] = '-';
+  }
+  
+  return str;
 }
 
 int main() {
