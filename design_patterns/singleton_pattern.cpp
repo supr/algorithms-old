42356385352309f47bcf49836a1efbaa04c56556
Gerald Stanje <Gerald.Stanje@gmail.com>
2015-01-01 23:58:08 +0100

    formatted code

--- design_patterns/singleton_pattern.cpp
+++ design_patterns/singleton_pattern.cpp
@@ -14,84 +14,79 @@ more efficiently when only one object exists, or that restrict the instantiation
 objects.
 */
 
-class Singleton
-{
+class Singleton {
 private:
-	static Singleton *inst_;
+  static Singleton *inst_;
 
 protected:
-	// default constructor: dont allow copies of the object (because its declared as protected)
-	Singleton() {}
+  // default constructor: dont allow copies of the object (because its declared as protected)
+  Singleton() {}
 
-	// move constructure: dont allow to use it (because its declared as protected)
-	Singleton(Singleton&& other);
+  // move constructure: dont allow to use it (because its declared as protected)
+  Singleton(Singleton&& other);
 
-  // move assignment operator: dont allow copies of the object (because its declared as protected)
-  Singleton& operator= (Singleton&& other);
+    // move assignment operator: dont allow copies of the object (because its declared as protected)
+    Singleton& operator= (Singleton&& other);
     
-	// copy-constructor: dont allow copies of the object (because its declared as protected) 
-	Singleton(const Singleton& other);
+  // copy-constructor: dont allow copies of the object (because its declared as protected) 
+  Singleton(const Singleton& other);
 
-	// assignment operator: dont allow copies of the object (because its declared as protected)
-	Singleton& operator= (const Singleton& other);
+  // assignment operator: dont allow copies of the object (because its declared as protected)
+  Singleton& operator= (const Singleton& other);
 
 public:
-	~Singleton() {
-		delete inst_;
-	}
+  ~Singleton() {
+    delete inst_;
+  }
 
-	static Singleton *get_Instance() {
-		if (inst_ == NULL)
-		{
-			inst_ = new Singleton;
-		}
+  static Singleton *get_Instance() {
+    if (inst_ == NULL)
+    {
+      inst_ = new Singleton;
+    }
 
-		return inst_;
-	}
+    return inst_;
+  }
 
-	void test() {
-		cout << "test" << endl;
-	}
+  void test() {
+    cout << "test" << endl;
+  }
 };
 
 Singleton* Singleton::inst_ = 0; // initialize pointer
 
-class Singleton2
-{
-
+class Singleton2 {
 public:
-	static Singleton2& Instance()
-	{
-		//das einzige Objekt dieser Klasse erzeugen und als Referenz zur�ckgeben 
-		static Singleton2 instance;
-		return instance;
-	}
+  static Singleton2& Instance() {
+    //das einzige Objekt dieser Klasse erzeugen und als Referenz zur�ckgeben 
+    static Singleton2 instance;
+    return instance;
+  }
 
-	void doSomething() { }
+  void doSomething() { }
 
 
 protected:
     // default constructor: dont allow copies of the object (because its declared as protected)
-	Singleton2() {}
+  Singleton2() {}
 
-	// move constructure: dont allow to use it (because its declared as protected)
-	Singleton2(Singleton2&& other);
+  // move constructure: dont allow to use it (because its declared as protected)
+  Singleton2(Singleton2&& other);
 
-  // move assignment operator: dont allow copies of the object (because its declared as protected)
-  Singleton2& operator= (Singleton2&& other);
+    // move assignment operator: dont allow copies of the object (because its declared as protected)
+    Singleton2& operator= (Singleton2&& other);
   
-	// copy-constructor: dont allow copies of the object (because its declared as protected) 
-	Singleton2(const Singleton2& other);
+  // copy-constructor: dont allow copies of the object (because its declared as protected) 
+  Singleton2(const Singleton2& other);
 
-	// assignment operator: dont allow copies of the object (because its declared as protected)
-	Singleton2& operator= (const Singleton2& other);
+  // assignment operator: dont allow copies of the object (because its declared as protected)
+  Singleton2& operator= (const Singleton2& other);
 
 };
 
-int main()
-{
-	Singleton *p1 = Singleton::get_Instance();
-	p1->test();
+int main() {
+  Singleton *p1 = Singleton::get_Instance();
+  p1->test();
 
-	return 0;
+  return 0;
 }
