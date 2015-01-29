49da89fca80e67c274bd0ed3e1ee23f7fd23b037
Gerald Stanje <Gerald.Stanje@gmail.com>
2015-01-28 22:48:26 -0500

    added new algo question

---
+++ basic_algo/anagram_groups.cpp
@@ -0,0 +1,12 @@
+/*
+Question:
+Given a list of strings, return a list of lists of strings that groups all anagrams. 
+
+Ex. given {trees, bike, cars, steer, arcs} 
+return { {cars, arcs}, {bike}, {trees, steer} } 
+
+m = # of words 
+n = length of longest word 
+
+I solved this in O(m * n * log n) time.
+*/
