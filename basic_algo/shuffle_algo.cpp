8ae4598d3b6ade920ffb1902d1a9ba03674913ec
Gerald Stanje <Gerald.Stanje@gmail.com>
2015-02-01 16:55:01 -0500

    implemented algo

--- basic_algo/shuffle_algo.cpp
+++ basic_algo/shuffle_algo.cpp
@@ -4,3 +4,27 @@ Write a method to shuffle a deck of cards. It must be a perfect shuffle - in oth
 each 52! permutations of the deck has to be equaliy likely. Assume that you are given a random 
 number generator which is perfect.
 */
+
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+void shuffle(vector<int> &vec) {
+  for (int i = vec.size() - 1; i > 0; i--) {
+    int r = rand() % i;
+
+    swap(vec[i], vec[r]);
+  }
+}
+
+int main() {
+  vector<int> vec = {1, 2, 3, 4, 5, 6, 7};
+
+  srand( time( NULL ) );
+  shuffle(vec);
+
+  for_each(vec.begin(), vec.end(), [](int val) { cout << val << ' '; });
+
+  return 0;
+}
