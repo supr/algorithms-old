562bf2c0c8f09853d16e71dc2cfc297faae4c8a9
Gerald Stanje <Gerald.Stanje@gmail.com>
2015-02-01 14:51:06 -0500

    added new algo

---
+++ basic_algo/shuffle_algo.cpp
@@ -0,0 +1,6 @@
+/*
+Question:
+Write a method to shuffle a deck of cards. It must be a perfect shuffle - in other words, 
+each 52! permutations of the deck has to be equaliy likely. Assume that you are given a random 
+number generator which is perfect.
+*/
