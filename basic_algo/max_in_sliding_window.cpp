7130ba6fc98db2e9b4f22de4c0ca45fff3074727
Gerald Stanje <Gerald.Stanje@gmail.com>
2015-02-06 23:52:44 +0100

    optimized algo

--- basic_algo/max_in_sliding_window.cpp
+++ basic_algo/max_in_sliding_window.cpp
@@ -3,6 +3,9 @@
 #include <list>
 #include <vector>
 #include <algorithm>
+#include <queue>
+#include <stack>
+using namespace std;
 using namespace std;
 
 /*
@@ -19,7 +22,8 @@ Sliding Windows in an Array		Maximums in Sliding Windows
 2,3,4,2,6,[2,5,1]                       5
 */
 
-vector<int> maxInWindows(vector<int> maxi, unsigned int windowSize) {
+// time complexity: O(n * k)
+vector<int> maxInWindow(vector<int> maxi, unsigned int windowSize) {
 	for(auto it = maxi.begin(); it != maxi.end(); it++) {
 		*it = *max_element(it, it + windowSize);
 		
@@ -32,14 +36,82 @@ vector<int> maxInWindows(vector<int> maxi, unsigned int windowSize) {
 	return maxi;
 }
 
+class node {
+public:
+  int index;
+  int value;
+
+  node(int index_, int value_): index(index_), value(value_) {}
+};
+
+struct Comparator {
+  bool operator()(const node &lhs, const node &rhs) {
+    return lhs.value < rhs.value;
+  }
+};
+
+typedef priority_queue<node, vector<node>, Comparator> pq;
+
+void removeIndex(pq &maxHeap, int index) {
+  stack<node> copy;
+  
+  while (!maxHeap.empty()) {
+    node n = maxHeap.top();
+    maxHeap.pop();
+    
+    if (n.index == index) {
+      break;
+    }
+    else {
+      copy.push(n);
+    }
+  }
+
+  while (!copy.empty()) {
+     maxHeap.push(copy.top());  
+     copy.pop();
+  }
+  
+}
+
+vector<int> maxInWindow2(vector<int> &vec, int k) {
+  vector<int> out;
+  pq maxHeap;
+
+  for (int i = 0; i < vec.size(); i++) {
+    if (i < k) {
+      maxHeap.push(node(i, vec[i]));
+      
+      if (i == (k - 1)) {
+        out.push_back(maxHeap.top().value);
+      }
+    }
+    else {
+      maxHeap.push(node(i, vec[i]));
+      removeIndex(maxHeap, i - k);
+      out.push_back(maxHeap.top().value);
+    }     
+  }
+
+  return out;
+}
+
 int main() {
 	// your code goes here
 	
 	vector<int> numbers = {2, 3, 4, 2, 6, 2, 5, 1};
-	vector<int> maxi = maxInWindows(numbers, 3);
+	vector<int> maxi = maxInWindow(numbers, 3);
 	
-	for(auto value : maxi)
+	for(auto value : maxi) {
 		cout << value << endl;
-	
+	}
+
+  vector<int> vec = {1,2,5,7,3,1,7,9,0};
+  vector<int> out = maxInWindow2(vec, 2);
+    
+  for (auto e : out) {
+    cout << e << ' ';
+  }
+
 	return 0;
 }
