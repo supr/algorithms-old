562918ddc4e0f3df8e4784dc84f0a7c27ba50a42
Gerald Stanje <Gerald.Stanje@gmail.com>
2015-02-15 21:45:19 +0100

    moved algo to string dir

--- string_algo/strstr.cpp
+++ string_algo/strstr.cpp
@@ -1,6 +1,15 @@
 #include <iostream>
 using namespace std;
 
+/*
+Question:
+Implement strstr() to Find a Substring in a String
+
+Knuth Morris Pratt Algorithm (KMP) runs in O(n)
+https://www.youtube.com/watch?v=SVKe7bvQ4Xk
+*/
+
+// complexity: O(n * m), n=length of str, m=length of search
 char *strstr(char *str, const char *search) {
 	unsigned int len = 0;
 	bool found = false;
@@ -36,10 +45,23 @@ char *strstr(char *str, const char *search) {
 	return NULL;
 }
 
+char *StrStr(const char *s1, const char *s2) {
+  const char *a = s1;
+  const char *b = s2;
+
+  for (;;) {
+    if (!*b) return (char *)s1;
+    if (!*a) return NULL;
+    if (*a++ == *b++) continue;
+    a = ++s1;
+    b = s2;
+  }
+}
+
 int main() {
 	// your code goes here
 	
-	char str[] = "This is a simple string";
+  char str[] = "This is a simple string";
   char *pch;
   pch = strstr(str, "simple");
   cout << pch << endl;
