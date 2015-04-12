1988bf1392a235994fcd3a1cd374bf4cdfc1cc1a
Gerald Stanje <Gerald.Stanje@gmail.com>
2015-04-12 23:22:25 +0200

    added new question

---
+++ graph_algo/max_sum_of_vertex_weights.cpp
@@ -0,0 +1,6 @@
+/*
+Question:
+Given a set of vertices V that describes a path in a graph, with each vertex assigned a weight. 
+Find a subset of V that maximizes the sum of vertex weights without any two vertices in that 
+subset being adjacent.
+*/
