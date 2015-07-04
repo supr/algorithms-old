7f6706112c7e414c033636cb9ce6dea9a6864c2f
Gerald Stanje <Gerald.Stanje@gmail.com>
2015-07-04 17:39:29 +0200

    added algorithms

---
+++ math/coins.cpp
@@ -0,0 +1,11 @@
+/*
+Question:
+Given some dollar value in cents (e.g. 200 = 2 dollars, 1000 = 10 dollars), find all the combinations of coins that make up 
+the dollar value. There are only penny, nickel, dime, and quarter. (quarter = 25 cents, dime = 10 cents, nickel = 5 cents, 
+penny = 1 cent)
+
+For example, if 100 was given, the answer should be...
+4 quarter(s) 0 dime(s) 0 nickel(s) 0 pennies
+3 quarter(s) 1 dime(s) 0 nickel(s) 15 pennies
+etc.
+*/
---
+++ math/football.cpp
@@ -0,0 +1,23 @@
+/*
+Question:
+given a number, calculate how many unique, order-independent ways there are to achieve a score of that number in American Football.
+
+Let's see how few characters can be used in each of your favorite languages! The usual rules apply. Input and output in a way 
+that is natural to your language.
+
+Recall that the ways to score in Football are:
+safety (2 points), 
+field goal (3 points), 
+touchdown (6 points),
+touchdown + extra point (7 points), 
+and touchdown + 2-point conversion (8 points).
+
+For example, given 9 as input, the program should return 4, since you could score:
+
+1 touchdown + extra point and 1 safety
+1 touchdown and 1 field goal
+3 field goals
+3 safeties and 1 field goal
+
+Note that order does not matter.
+*/
